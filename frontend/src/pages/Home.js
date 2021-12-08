d9157b6144ab27b73f8f5c330ba69ef3c0bf3f04
theodmt <theo.dumont@edu.ece.fr>
2021-12-08 12:03:46 +0100

    v5

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,14 +10,19 @@ export default class Home extends React.Component{
                 <img class="logo_home"src="Logo_Home.png" alt="logo_home"/>
 
                 <div class="home_boutton">
+                    <p class="title_ongletchimie">Chimie</p>
                     <a href="#" class="home_boutton_ongletchimie">
                     </a>
+                    <p class="title_ongletmaths">Mathématiques</p>
                     <a href="#" class="home_boutton_ongletmaths">
                     </a>
+                    <p class="title_ongletmethodologie">Méthodologie</p>
                     <a href="#" class="home_boutton_ongletmethodologie">
                     </a>
+                    <p class="title_ongletphysique">Physique</p>
                     <a href="#" class="home_boutton_ongletphysique">
                     </a>
+                    <p class="title_ongletprogrammation">Programmation</p>
                     <a href="#" class="home_boutton_ongletprogrammation">
                     </a>
                 </div>
