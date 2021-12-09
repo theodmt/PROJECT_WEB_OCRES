9e04bae102fb787b6ea348cd5984f4091ac8353b
AntoineRuvicini <antoine.ruvi@gmail.com>
2021-12-09 17:44:47 +0100

    V4

---
+++ frontend/src/pages/Nosprofils.js
@@ -0,0 +1,23 @@
+import react from 'react';
+
+function Avis() {
+    return (
+    <>
+        <div className='nosprofil'>
+            <h1>Nos profils</h1>
+            <article>
+                <img class="photo_profil" src="" alt=""/>
+                <div class="info_profils">
+
+                </div>
+                <div class="">
+
+                </div>
+            </article>
+        </div>
+    </>
+    
+    );
+}
+
+export default Avis; 
