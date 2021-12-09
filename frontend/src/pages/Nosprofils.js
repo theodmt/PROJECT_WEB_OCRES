cfd056396f30bd0231431e874931bf23e5dfe533
AntoineRuvicini <antoine.ruvi@gmail.com>
2021-12-09 17:52:01 +0100

    v6

--- frontend/src/pages/Nosprofils.js
+++ frontend/src/pages/Nosprofils.js
@@ -6,12 +6,15 @@ function Avis() {
         <div className='nosprofil'>
             <h1>Nos profils</h1>
             <article>
-                <img class="photo_profil" src="" alt=""/>
+                <img class="photo_profil" src="{this.image}" alt=""/>
                 <div class="info_profils">
 
                 </div>
-                <div class="">
-
+                <div class="parcours_container">
+                    <h1>Mon parcours</h1>
+                    <p class="parcours_content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada eget elit ac tincidunt. Curabitur eget magna porttitor urna semper varius vitae nec felis. Aenean commodo ultricies enim, non efficitur urna pellentesque eget. Suspendisse finibus orci eleifend augue tincidunt egestas. Cras et tortor egestas, luctus mauris sit amet, tempor libero. Fusce ut augue sed ligula ultrices hendrerit. Praesent nisl eros, rutrum et suscipit eu, viverra eu nulla. Maecenas vestibulum commodo posuere. Integer facilisis egestas nunc, ac viverra nisi vehicula non. Donec imperdiet odio eu sem cursus, varius convallis justo mollis. Pellentesque volutpat quam sagittis ex ultrices malesuada. Sed auctor nunc in erat convallis, sit amet ornare justo aliquet. Nunc pulvinar ligula ultrices laoreet vehicula.
+                    </p>
                 </div>
             </article>
         </div>
