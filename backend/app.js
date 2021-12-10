79729c5f9bad6fb8397dea5ba63e82f212bb9829
AntoineRuvicini <antoine.ruvi@gmail.com>
2021-12-10 12:28:28 +0100

    avant derniere

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 var express = require("express");
+
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+var indexRouter = require("./index");
 var usersRouter = require("./routes/users");
 
+
 var app = express();
+
+app.use(cors());
+
+
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,5 +21,6 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
 
 module.exports = app;
